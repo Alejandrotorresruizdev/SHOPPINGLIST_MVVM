bb0237d86ca8d03c8723d0d73ba3a8fa802f35b7
Alejandro Torres <alejandrojuegoselx@gmail.com>
2020-10-06 11:51:06 +0200

    [ADD] Created shopping repository

--- src/main/java/com/example/shoppinglist/data/db/ShoppingDatabase.kt
+++ src/main/java/com/example/shoppinglist/data/db/ShoppingDatabase.kt
@@ -1,9 +1,10 @@
-package com.example.shoppinglist
+package com.example.shoppinglist.data.db
 
 import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.shoppinglist.data.db.entities.ShoppingItem
 
 @Database(
     entities = [ShoppingItem::class],
@@ -16,8 +17,12 @@ abstract class ShoppingDatabase : RoomDatabase(){
     companion object {
         private var instance : ShoppingDatabase? = null
         private val LOCK = Any()
-        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
-            instance ?: createDatabase(context).also{
+        operator fun invoke(context: Context) = instance
+            ?: synchronized(LOCK) {
+            instance
+                ?: createDatabase(
+                    context
+                ).also{
                 instance = it
             }
         }
